47f61fa5520eb366bb91752cd399e56b8351db7a
arif <arif@themascoteers.com>
2023-07-12 13:25:33 +1000

    done

--- traditional_machine.py
+++ traditional_machine.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+from torchvision.models import resnet50, ResNet50_Weights
 
 
 class TraditionalMachine(nn.Module):
     def __init__(self):
         super().__init__()
-        self.resnet = torchvision.models.resnet18(pretrained=True)
+        self.resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
         number_input = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(number_input, 2)
 
